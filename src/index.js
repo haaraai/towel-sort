41b0783939c36250b91249b433986e68762d6953
Yahor Harai <egor.garai@gmail.com>
2022-08-09 01:14:22 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 module.exports = function towelSort (matrix) {
 	let sorted = [];
 	for(key in matrix){
-		if (key % 2 == 0) sorted.push(...matrix[key]);
+		if (key % 2 === 0) sorted.push(...matrix[key]);
 		else sorted.push(...matrix[key].reverse());
 	}
   return sorted;
